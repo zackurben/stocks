5b884e0affdfea98ef991c267ce1af465b6604b5
Zack Urben <zackurben@gmail.com>
2018-04-16 19:40:53 -0500

    Updating the tools.

---
+++ src/tools.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const path = require('path');
+const _ = require('lodash');
+const chalk = require('chalk');
+const fs = require('fs');
+const yahoo = require('yahoo-stocks');
+
+module.exports = (cli, options) => {
+  const getPortfolio = () =>
+    new Promise(resolve => {
+      let portfolio;
+
+      try {
+        portfolio = require(options.portfolio);
+      } catch (e) {
+        // Create a new portfolio if one didn't exist.
+        portfolio = require(path.join(__dirname, '../', 'example.json'));
+        return savePortfolio(portfolio);
+      }
+
+      return resolve(portfolio);
+    });
+
+  const savePortfolio = portfolio =>
+    new Promise((resolve, reject) => {
+      fs.writeFile(
+        options.portfolio,
+        JSON.stringify(portfolio, null, 4),
+        err => {
+          if (err) {
+            return reject(err);
+          }
+
+          return resolve(portfolio);
+        }
+      );
+    });
+
+  const getSymbol = (symbol = yahoo.lookup(symbol));
+
+  const displayPortfolio = portfolio =>
+    new Promise((resolve, reject) => {
+      const stocks = Object.keys(portfolio.stocks || {});
+    });
+
+  /**
+   * A simple error handler for logging issues.
+   *
+   * @param msg {string}
+   *   The error message to output/track.
+   * @param fatal {boolean}
+   *   If this error is fatal or not (program will exit if fatal).
+   */
+  const error = (msg, fatal) => {
+    fatal = typeof fatal !== 'boolean' ? false : fatal;
+
+    // Show the user the error and potentially exit.
+    cli.error(msg);
+    if (fatal) {
+      return cli.exit();
+    }
+  };
+
+  return {
+    cli,
+    options,
+    getPortfolio,
+    savePortfolio,
+    getSymbol,
+    displayPortfolio,
+    error,
+    chalk
+  };
+};
